9c07c5e57799ae09b4f307a903053a64f9d6fd7d
Swagata-codearts <swagata.codearts@gmail.com>
2022-07-05 16:58:16 +0530

    Login header structure

--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -12,7 +12,7 @@ export class SplashPage implements OnInit {
     public router: Router
   ) { 
     setTimeout(() => {
-      this.router.navigateByUrl('home')
+      this.router.navigateByUrl('login')
     }, 3000);
   }
 
