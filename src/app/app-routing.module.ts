c8f003903061fe393a5fe5661d63df6131c0f49a
Dipan-25 <dipan.codearts@gmail.com>
2022-07-05 15:34:16 +0530

    Add Splash Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'splash',
+    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
   },
+
 ];
 
 @NgModule({
