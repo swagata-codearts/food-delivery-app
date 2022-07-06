b85ec229db65901f9f532a4e26656e8d7dd6d423
Swagata-codearts <swagata.codearts@gmail.com>
2022-07-06 13:51:41 +0530

    Env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://mx2test2.com/restaurant-appwp-json'
 };
 
 /*
