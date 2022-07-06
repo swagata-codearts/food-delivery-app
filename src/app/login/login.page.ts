dfcdde0065b4a1e7f74499ae5be3b1a26bdd1033
Shourya Chowdhury <shouryachowdhury@Shouryas-MacBook-Air.local>
2022-07-06 13:44:17 +0530

    added code for login with wordpress

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AlertController, ToastController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,92 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  userForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private api: ApiService,
+    private fb: FormBuilder,
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController,
+    private storage: Storage
+    ) { }
 
   ngOnInit() {
+    this.storage.create();
+    this.userForm = this.fb.group({
+      username: ['', Validators.required],
+      email: '',
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    this.api.signIn(this.userForm.value.username, this.userForm.value.password).subscribe(
+      res => {
+        console.log(res)
+      },
+      err => {
+        this.showError(err);
+      }
+    );
+  }
+ 
+ 
+
+  async openPwReset() {
+    const alert = await this.alertCtrl.create({
+      header: 'Forgot password?',
+      message: 'Enter your email or username to retrieve a new password',
+      inputs: [
+        {
+          type: 'text',
+          name: 'usernameOrEmail'
+        }
+      ],
+      buttons: [
+        {
+          role: 'cancel',
+          text: 'Back'
+        },
+        {
+          text: 'Reset Password',
+          handler: (data) => {
+            this.resetPw(data['usernameOrEmail']);
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  }
+ 
+  resetPw(usernameOrEmail) {
+    this.api.resetPassword(usernameOrEmail).subscribe(
+      async res => {
+        const toast = await this.toastCtrl.create({
+          message: res['message'],
+          duration: 2000
+        });
+        toast.present();
+      },
+      err => {
+        this.showError(err);
+      }
+    );
+  }
+ 
+  
+
+ 
+  async showError(err) {
+    
+    const alert = await this.alertCtrl.create({
+      header: err.error.code,
+      subHeader: err.error.data.status,
+      message: err.error.message,
+      buttons: ['OK']
+    });
+    await alert.present();
   }
 
 }
