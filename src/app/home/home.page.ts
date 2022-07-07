424c2be5b955155aeb25802763d69835193be7ac
Swagata-codearts <swagata.codearts@gmail.com>
2022-07-07 16:16:41 +0530

    Category onclick events

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,11 +8,28 @@ import { ProductService } from '../services/product.service';
 })
 export class HomePage {
 
+  public allClicked: boolean = true;
+  public lunchClicked: boolean = false;
+  public dinnerClicked: boolean = false;
+  public fastFoodClicked: boolean = false;
+  public dessertClicked: boolean = false;
+
   constructor(private productService:ProductService) {}
+
   ngOnInit() {
     this.productService.getProducts().subscribe(res=>{
       console.log(res)
     })
   }
-
+  
+  
+  public onAllClick() {
+    this.allClicked = !this.allClicked;
+  }
+  public onLunchClick() {
+    this.allClicked = !this.allClicked;
+  }
+  public onDinnerClick() {this.dinnerClicked = !this.dinnerClicked;}
+  public onFastFoodClick() {this.fastFoodClicked = !this.fastFoodClicked;}
+  public onDessertClick() {this.dessertClicked = !this.dessertClicked;}
 }
