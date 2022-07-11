9282971a88dfa06481e37b3154f70a4f6fa6807d
Swagata-codearts <swagata.codearts@gmail.com>
2022-07-11 15:18:31 +0530

    Add increment decrement functions

--- src/app/product-inner/product-inner.page.ts
+++ src/app/product-inner/product-inner.page.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-inner.page.scss'],
 })
 export class ProductInnerPage implements OnInit {
-
+  public currentNumber = 1;
+  public productPrice = 65;
+  updatedProductPrice = this.productPrice
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  public increment () {
+   
+    this.currentNumber++;
+    this.updatedProductPrice = this.currentNumber * this.productPrice
+  }
+  
+  public decrement () {
+    if (this.currentNumber > 0) {
+      this.currentNumber--;
+      this.updatedProductPrice = this.currentNumber * this.productPrice
+    } else  {
+      this.currentNumber = 0;
+    }
+  }
+  
 }
